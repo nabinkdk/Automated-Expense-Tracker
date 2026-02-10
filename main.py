44e909e868c23fae4c56fd6be0d1f278659c7d9d
Nabin Khadka <khadkanabin090@gmail.com>
2026-02-10 21:20:33 +0000

    created a class Expense

---
+++ main.py
@@ -0,0 +1,9 @@
+class Expense:
+    def __init__(self, date, title, expense):
+        self.date = date
+        self.title = title
+        self.expense = expense
+
+class ExpenseTracker:
+    def __init__(self):
+        self.expenses=[]
