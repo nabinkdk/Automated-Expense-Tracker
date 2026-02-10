6f5b8e6070bf294592ea3a79e5250a0ff6f5e8a5
Nabin Khadka <khadkanabin090@gmail.com>
2026-02-10 21:27:18 +0000

    created a method to remove the expenses

--- main.py
+++ main.py
@@ -12,4 +12,10 @@ class ExpenseTracker:
     def add_expense(self, expense):
         self.expenses.append(expense)
     
-    
+    def remove_expense(self,index):
+        if index>=0 and index<len(self.expenses):
+            del self.expenses[index]
+            print("Expense removed Successfully")
+        else:
+            print("Invalid expense index.")
+         
