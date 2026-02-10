8a7c85852e70da9376cdd2656f2ed470d30deff0
Nabin Khadka <khadkanabin090@gmail.com>
2026-02-10 21:46:19 +0000

    created a main function where every choice is given to user to do

--- main.py
+++ main.py
@@ -32,3 +32,38 @@ class ExpenseTracker:
     def total_expense(self):
         total = sum(amt.expense for amt in self.expenses)
         print(f"Total Expenses: ${total:.2f}")
+
+
+def main():
+    tracker = ExpenseTracker()
+    while True:
+        print("\nExpense Tracker Menu:")
+        print("1. Add Expenses")
+        print("2. Remove Expenses")
+        print("3. View Expenses")
+        print("4. Total Expenses")
+        print("5. Exit")
+
+        choice = int(input("Enter a choice(1-5):\t"))
+        if choice == 1:
+            date = input("Enter the date (YYYY-MM-DD): ")
+            title = input("Enter a Title: ")
+            amt = float(input("Enter the amount: "))
+            expense = Expense(date, title, amt)
+            tracker.add_expense(expense)
+        elif choice == 2:
+            index = int(input("Enter a index to remove"))
+            tracker.remove_expense(index - 1)
+
+        elif choice == 3:
+            tracker.view_expense()
+        elif choice == 4:
+            tracker.total_expense()
+        elif choice == 5:
+            print("Exiting .....!")
+            break
+        else:
+            print("Invalid choice")
+
+if __name__=="__main__":
+    main()
