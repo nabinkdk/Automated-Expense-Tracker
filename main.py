9b9b5e25c202ecc11e294a03205584534f8ebbef
Nabin Khadka <khadkanabin090@gmail.com>
2026-02-10 21:32:25 +0000

    created a method to view expense

--- main.py
+++ main.py
@@ -11,11 +11,21 @@ class ExpenseTracker:
 
     def add_expense(self, expense):
         self.expenses.append(expense)
-    
-    def remove_expense(self,index):
-        if index>=0 and index<len(self.expenses):
+
+    def remove_expense(self, index):
+        if index >= 0 and index < len(self.expenses):
             del self.expenses[index]
             print("Expense removed Successfully")
         else:
             print("Invalid expense index.")
-         
+
+    def view_expense(self):
+        if len(self.expenses) == 0:
+            print("No expenses Found !")
+        else:
+            print("Expense List:")
+            for i, expense in enumerate(self.expenses, start=1):
+                print(
+                    f"{i}. Date:{expense.date}, Title:{expense.title}, Amount:{expense.expense}"
+                )
+    
